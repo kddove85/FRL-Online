c87b04db94b6697ae8417840a4bd7902f5a19815
unknown <kddove85@gmail.com>
2022-05-02 11:17:44 -0400

    Updated model to include day of the week

--- app/models.py
+++ app/models.py
@@ -9,15 +9,25 @@ class Station(models.Model):
         return self.call_sign
 
 
+class Weekday(models.Model):
+    day = models.CharField(max_length=10)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.day
+
+
 class ScheduledItemManager(models.Manager):
-    def create_request(self, url, output_name, start_time, end_time):
-        request = self.create(url=url, output_name=output_name, start_time=start_time, end_time=end_time)
+    def create_request(self, url, output_name, day_of_the_week, start_time, end_time):
+        request = self.create(url=url, output_name=output_name, day_of_the_week=day_of_the_week, start_time=start_time,
+                              end_time=end_time)
         return request
 
 
 class ScheduledItem(models.Model):
     url = models.URLField()
     output_name = models.CharField(max_length=255)
+    day_of_the_week = models.IntegerField()
     start_time = models.CharField(max_length=8)
     end_time = models.CharField(max_length=8)
 
--- app/views.py
+++ app/views.py
@@ -2,25 +2,41 @@ from django.shortcuts import render
 from urllib import parse
 from .models import Station
 from .models import ScheduledItem
+from .models import Weekday
 from .recording_logic import recording_logic
-import threading
 
 
 def index(request):
+    stations = Station.objects.order_by('call_sign')
+    days = Weekday.objects.order_by('value')
+    context = {"stations": stations, "days": days}
+
     if request.method == 'POST':
-        stations = Station.objects.order_by('call_sign')
         query_string = request.body.decode('utf8')
         payload = dict(parse.parse_qsl(query_string))
-        item = ScheduledItem.objects.create_request(payload['radioStation'], payload['fileOutputName'],
-                                                    payload['startTime'], payload['endTime'])
-        item.save()
-        context = {"stations": stations, 'message': "Recording Request Submitted"}
+        payload = process_payload(payload)
+        for day in payload['days']:
+            item = ScheduledItem.objects.create_request(payload['radioStation'], payload['fileOutputName'], day,
+                                                        payload['startTime'], payload['endTime'])
+            item.save()
+        context["message"] = "Recording Request Submitted"
         return render(request, 'app/index.html', context)
 
     if request.method == 'GET':
-        stations = Station.objects.order_by('call_sign')
-        context = {"stations": stations}
         return render(request, 'app/index.html', context)
+
+
+def process_payload(payload):
+    processed_payload = payload
+    days = []
+    for x in range(0, 7):
+        try:
+            processed_payload.pop(str(x))
+            days.append(x)
+        except KeyError:
+            continue
+    processed_payload["days"] = days
+    return processed_payload
 
 
 def process_request(payload):
